241e2c7f2879999c2e5586032d732437b6ec2b4a
pattty847 <pattty847@gmail.com>
2019-02-06 21:49:07 -0500

    Find line

--- MathOPS/src/mathops/MathOPS.java
+++ MathOPS/src/mathops/MathOPS.java
@@ -18,7 +18,7 @@ public class MathOPS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        doubleNum(14, 1000);
+        findLineEquation();
     }
 
     public static void doubleNum(int num, int times) {
@@ -41,16 +41,35 @@ public class MathOPS {
             x2 = Double.parseDouble(input.nextLine());
             System.out.println("Enter y2: ");
             y2 = Double.parseDouble(input.nextLine());
+            
+            m = (y2 - y1) / (x2 - x1);
+            b = y1 - m * x1;
+            
+            System.out.println("Slope: " + m);
+            System.out.println("Y-intercept: " + b);
+            System.out.println("y = " + m + "(x)" + "+" + b);
+            System.out.println(y1 + " = " + m + "(" + x1 + ")" + "+" + b);
         } catch (NumberFormatException e) {
             System.out.println("Enter only integers.");
         }
-        m = (y2 - y1) / (x2 - x1);
-        // y - k = m (x - h).
-        System.out.println("Slope: " + m);
-        b = y1 - m * x1;
-        System.out.println("Y-intercept: " + b);
-        System.out.println("y = " + m + "(x)" + "+" + b);
-        System.out.println(y1 + " = " + m + "(" + x1 + ")" + "+" + b);
     }
-
+    
+    public static void foil() {
+        double x1 = 0, y1 = 0, x2 = 0, y2 = 0, m = 0, b = 0;
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.println("Enter x1: ");
+            x1 = Double.parseDouble(input.nextLine());
+            System.out.println("Enter y1: ");
+            y1 = Double.parseDouble(input.nextLine());
+            System.out.println("Enter x2: ");
+            x2 = Double.parseDouble(input.nextLine());
+            System.out.println("Enter y2: ");
+            y2 = Double.parseDouble(input.nextLine());
+            
+            System.out.println("(");
+        }catch(NumberFormatException e) {
+            System.out.println("Enter only integers.");
+        }
+    }
 }
