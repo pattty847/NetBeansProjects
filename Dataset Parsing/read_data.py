e06537abfa5ea2265bf6c3b520bee1d80aa13c56
pattty847 <pattty847@gmail.com>
2019-09-06 13:29:24 -0400

    Pandas DataFrame

--- Dataset Parsing/read_data.py
+++ Dataset Parsing/read_data.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import pandas as pd
 
 # ['video_id', 'trending_date', 'title', 'channel_title', 'category_id', 'publish_time',
 # 'tags', 'views', 'likes', 'dislikes', 'comment_count', 'thumbnail_link', 'comments_disabled', 
@@ -9,14 +10,17 @@ import csv
 def open_file():
     file  = 'USvideos.csv'
     
-    with open(file, 'r') as f:
-        reader = csv.reader(f, delimiter=',')
-        headers = next(reader)
-        data = list(reader)
-        data = np.array(data)
+    data = pd.read_csv(file)
+    # print(data.sort_values(by='video_error_or_removed', ascending=False))
     
+    info = (data.drop(['video_id', 'thumbnail_link'], axis=1))
 
-    print(headers)
+    info = info[:20].sort_values(by='views')
+    print(info['views'])
+
+    # info.plot(kind='line', x='views', y='comment_count', color='red')
+
+    plt.show()
 
 
 if __name__ == "__main__":
