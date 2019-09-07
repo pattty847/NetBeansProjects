845d704fe6d9b605331e5fd2204924a41feec008
pattty847 <pattty847@gmail.com>
2019-09-07 19:36:50 -0400

    Upload

--- Python/Dataset Parsing/read_data.py
+++ Python/Dataset Parsing/read_data.py
@@ -1,27 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import seaborn
 import pandas as pd
 
-# ['video_id', 'trending_date', 'title', 'channel_title', 'category_id', 'publish_time',
-# 'tags', 'views', 'likes', 'dislikes', 'comment_count', 'thumbnail_link', 'comments_disabled', 
-# 'ratings_disabled', 'video_error_or_removed', 'description']
+
+# np.array[row, col]
 
 def open_file():
-    file  = 'USvideos.csv'
+    file  = r'C:\Users\pattt\Documents\NetBeansProjects\Python\Dataset Parsing\train.csv'
     
-    data = pd.read_csv(file)
-    # print(data.sort_values(by='video_error_or_removed', ascending=False))
-    
-    info = (data.drop(['video_id', 'thumbnail_link'], axis=1))
+    with open(file, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        # get header from first row
+        headers = next(reader)
+        # get all the rows as a list
+        data = list(reader)
+        # transform data into numpy array
+        data = np.array(data)
 
-    info = info[:20].sort_values(by='views')
-    print(info['views'])
+    data[1:20, 5].sort()
 
-    # info.plot(kind='line', x='views', y='comment_count', color='red')
-
+    plt.scatter(data[1:20, 5], data[1:20, 1])
     plt.show()
-
 
 if __name__ == "__main__":
     open_file()
