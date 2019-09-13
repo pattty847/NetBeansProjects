ceeb1891709440a625f6a6aa4377c40ea3815544
pattty847 <pattty847@gmail.com>
2019-09-13 11:34:30 -0400

    Titanic Dataset

--- Python/Dataset Parsing/read_data.py
+++ Python/Dataset Parsing/read_data.py
@@ -18,11 +18,44 @@ if __name__ == "__main__":
     test = pd.read_csv('test.csv')
     train = pd.read_csv('train.csv')
 
+    title_mapping = {"Mr": 0, "Miss": 1, "Mrs": 2, 
+                 "Master": 3, "Dr": 3, "Rev": 3, "Col": 3, "Major": 3, "Mlle": 3,"Countess": 3,
+                 "Ms": 3, "Lady": 3, "Jonkheer": 3, "Don": 3, "Dona" : 3, "Mme": 3,"Capt": 3,"Sir": 3 }
+
+    sex_mapping = {"female": 0, "male": 1}
+
     # bar_chart('Embarked')
 
     train_test_data = [train, test] # combining train and test dataset
 
     for dataset in train_test_data:     # loop through both datasets
+
+        # (' ([A-Za-z]+)/.') <-- find all letters a-z or A-Z that end with a (.)
         dataset['Title'] = dataset['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
 
-    bar_chart('SibSp')
+        # create a new column that maps their titles to a 1, 2, or 3
+        dataset['Title'] = dataset['Title'].map(title_mapping)
+
+        # map their sex to a 0 or 1
+        dataset['Sex'] = dataset['Sex'].map(sex_mapping)
+
+        dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0,
+        dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 26), 'Age'] = 1,
+        dataset.loc[(dataset['Age'] > 26) & (dataset['Age'] <= 36), 'Age'] = 2,
+        dataset.loc[(dataset['Age'] > 36) & (dataset['Age'] <= 62), 'Age'] = 3,
+        dataset.loc[ dataset['Age'] > 62, 'Age'] = 4
+
+        dataset['Embarked'] = dataset['Embarked'].fillna('S')
+
+    # fill all NaNs with a median age for their title across the whole dataset (title_mapping)
+    train["Age"].fillna(train.groupby("Title")["Age"].transform("median"), inplace=True)
+    test["Age"].fillna(test.groupby("Title")["Age"].transform("median"), inplace=True)
+
+    train["Fare"].fillna(train.groupby("Pclass")["Fare"].transform("median"), inplace=True)
+    test["Fare"].fillna(test.groupby("Pclass")["Fare"].transform("median"), inplace=True)
+
+    # get rid of the column 'Name'
+    test.drop('Name', axis=1, inplace=True)
+    train.drop('Name', axis=1, inplace=True)
+
+    bar_chart('Age')
