e043c77db208a9273318d69ee6dfcef89f1d3b47
pattty847 <pattty847@gmail.com>
2019-09-12 21:12:15 -0400

    Titanic update

--- Python/Dataset Parsing/read_data.py
+++ Python/Dataset Parsing/read_data.py
@@ -1,28 +1,28 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import csv
-import seaborn
+import seaborn as sns
+sns.set() # setting seaborn default for plots
 import pandas as pd
 
+# .isnull().sum()
+# 
 
-# np.array[row, col]
-
-def open_file():
-    file  = r'C:\Users\pattt\Documents\NetBeansProjects\Python\Dataset Parsing\train.csv'
-    
-    with open(file, 'r') as f:
-        reader = csv.reader(f, delimiter=',')
-        # get header from first row
-        headers = next(reader)
-        # get all the rows as a list
-        data = list(reader)
-        # transform data into numpy array
-        data = np.array(data)
-
-    data[1:20, 5].sort()
-
-    plt.scatter(data[1:20, 5], data[1:20, 1])
-    plt.show()
+def bar_chart(feature):
+    survived = train[train['Survived']==1][feature].value_counts()      # holds values where Survived==1 AND y is the feature
+    dead = train[train['Survived']==0][feature].value_counts()      # holds values where Survived==1 AND y is the feature
+    df = pd.DataFrame([survived,dead])      # DataFrame var with [survived, dead] as its (x) components
+    df.index = ['Survived','Dead']      # Create titles for each 
+    df.plot(kind='bar',stacked=True, figsize=(10,5))        # create a stacked bar graph
+    plt.show()      # show the graph
 
 if __name__ == "__main__":
-    open_file()
+    test = pd.read_csv('test.csv')
+    train = pd.read_csv('train.csv')
+
+    # bar_chart('Embarked')
+
+    train_test_data = [train, test] # combining train and test dataset
+
+    for dataset in train_test_data:     # loop through both datasets
+        dataset['Title'] = dataset['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
+
+    bar_chart('SibSp')
